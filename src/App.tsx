c1ed125f789c61e840594a1a40b3ff033d58b8f8
Roy QIU <karoyqiu@gmail.com>
2023-11-27 12:18:05 +0800

    加了个基础结构。

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { appWindow } from '@tauri-apps/api/window';
 import React from 'react';
 import useSubscribe from './api/useSubscribe';
-import EndpointList from './components/EndpointList';
-import SubscriptionDialog from './components/SubscriptionDialog';
-import SubscriptionList from './components/SubscriptionList';
+import ConnectionState from './components/ConnectionState';
+import Speedometer from './components/Speedometer';
 import db from './db';
 import { Subscription } from './db/subscription';
 
@@ -28,37 +27,53 @@ function App() {
   }, []);
 
   return (
-    <div role="tablist" className="tabs tabs-lifted grid-rows-[min-content_1fr] h-screen">
-      <input
-        role="tab"
-        name="main-tab"
-        className="tab"
-        type="radio"
-        aria-label="Endpoints"
-        defaultChecked
-      />
-      <div
-        role="tabpanel"
-        className="tab-content bg-base-100 border-base-300 h-full overflow-y-auto"
-      >
-        <div className="join">
-          <button className="btn btn-square join-item">Test</button>
+    <div className="flex flex-1">
+      <div className="flex flex-col w-48 bg-neutral">
+        <div className="p-6">
+          <Speedometer download={0} upload={0} />
         </div>
-        <EndpointList />
+        <div className="divider my-0" />
+        <nav className="join join-vertical flex-1">
+          <button className="btn btn-ghost join-item">General</button>
+        </nav>
+        <div className="divider my-0" />
+        <div className="p-4">
+          <ConnectionState connected={false} seconds={12345} />
+        </div>
       </div>
-      <input role="tab" name="main-tab" className="tab" type="radio" aria-label="Subscriptions" />
-      <div
-        role="tabpanel"
-        className="tab-content bg-base-100 border-base-300 h-full overflow-y-auto"
-      >
-        <SubscriptionList />
-      </div>
-      <button className="btn btn-ghost btn-sm" onClick={() => ref.current?.showModal()}>
-        <span className="material-symbols-outlined">add</span>
-        Subscribe
-      </button>
-      <SubscriptionDialog ref={ref} onClose={addSub} sub={{ name: '', url: '' }} />
+      <div className="flex-1 overflow-y-auto"></div>
     </div>
+    // <div role="tablist" className="tabs tabs-lifted grid-rows-[min-content_1fr] h-screen">
+    //   <input
+    //     role="tab"
+    //     name="main-tab"
+    //     className="tab"
+    //     type="radio"
+    //     aria-label="Endpoints"
+    //     defaultChecked
+    //   />
+    //   <div
+    //     role="tabpanel"
+    //     className="tab-content bg-base-100 border-base-300 h-full overflow-y-auto"
+    //   >
+    //     <div className="join">
+    //       <button className="btn btn-square join-item">Test</button>
+    //     </div>
+    //     <EndpointList />
+    //   </div>
+    //   <input role="tab" name="main-tab" className="tab" type="radio" aria-label="Subscriptions" />
+    //   <div
+    //     role="tabpanel"
+    //     className="tab-content bg-base-100 border-base-300 h-full overflow-y-auto"
+    //   >
+    //     <SubscriptionList />
+    //   </div>
+    //   <button className="btn btn-ghost btn-sm" onClick={() => ref.current?.showModal()}>
+    //     <span className="material-symbols-outlined">add</span>
+    //     Subscribe
+    //   </button>
+    //   <SubscriptionDialog ref={ref} onClose={addSub} sub={{ name: '', url: '' }} />
+    // </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import 'material-symbols/outlined.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

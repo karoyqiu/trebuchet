4d4da703c46537c29b54c9a81eef70e583c254c3
Roy QIU <karoyqiu@gmail.com>
2023-11-28 10:39:49 +0800

    节点列表添加选择功能。

--- src/components/EndpointList.tsx
+++ src/components/EndpointList.tsx
@@ -1,23 +1,35 @@
 import { useLiveQuery } from 'dexie-react-hooks';
+import React from 'react';
 import db from '../db';
 
 export default function EndpointList() {
   const items = useLiveQuery(() => db.endpoints.toArray(), []) ?? [];
+  const [selected, setSelected] = React.useState('');
 
   return (
     <table className="table">
       <tbody>
-        {items.map((item) => (
-          <tr key={`${item.host}:${item.port}`} className="hover">
-            <td className="w-full">
-              <p className="text-lg font-bold">{item.name}</p>
-              <p className="text-sm opacity-50">{`${item.host}:${item.port}`}</p>
-            </td>
-            <td>
-              <div className="badge">{item.protocol}</div>
-            </td>
-          </tr>
-        ))}
+        {items.map((item) => {
+          const key = `${item.host}:${item.port}`;
+
+          return (
+            <tr
+              key={key}
+              className={
+                selected === key ? 'bg-accent text-accent-content' : 'hover cursor-pointer'
+              }
+              onClick={() => setSelected(key)}
+            >
+              <td className="w-full">
+                <p className="text-lg font-bold">{item.name}</p>
+                <p className="text-sm opacity-50">{`${item.host}:${item.port}`}</p>
+              </td>
+              <td>
+                <div className="badge">{item.protocol}</div>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );

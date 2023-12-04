7a2752b2737f4783dcde72cd6c5c5b1cffacec13
Roy QIU <karoyqiu@gmail.com>
2023-12-04 15:15:28 +0800

    定时测延迟。

--- src/api/useSubscribe.ts
+++ src/api/useSubscribe.ts
@@ -1,16 +1,27 @@
 import { useEffect } from 'react';
+import db from '../db';
+import { testLatencies } from './endpointTest';
 import settings from './settings';
 import { updateSubscriptions } from './subscription';
 
 /** 定时更新订阅 */
 const useSubscribe = () => {
-  const { subUpdateInterval } = settings.use();
+  const { subUpdateInterval, epTestInterval } = settings.use();
 
   useEffect(() => {
     console.log(`Updating subscriptions every ${subUpdateInterval} minutes.`);
     const timer = setInterval(updateSubscriptions, subUpdateInterval * 60 * 1000);
     return () => clearInterval(timer);
   }, [subUpdateInterval]);
+
+  useEffect(() => {
+    console.log(`Testing latency every ${epTestInterval} minutes.`);
+    const timer = setInterval(async () => {
+      const all = await db.endpoints.toArray();
+      await testLatencies(all);
+    }, epTestInterval * 60 * 1000);
+    return () => clearInterval(timer);
+  }, [epTestInterval]);
 };
 
 export default useSubscribe;

edacad74c5e053d9a726d53581c45d502aa60888
Roy QIU <karoyqiu@gmail.com>
2023-12-05 12:40:52 +0800

    禁用定时更新订阅，改为常规测速超时后自动更新。

--- src/api/useSubscribe.ts
+++ src/api/useSubscribe.ts
@@ -1,21 +1,19 @@
 import { useEffect } from 'react';
-import db from '../db';
-import { selectFastest } from './currentEndpoint';
-import { testLatencies, testLatency } from './endpointTest';
+import { testLatency } from './endpointTest';
 import settings from './settings';
 import { updateSubscriptions } from './subscription';
 
 /** 定时更新订阅 */
 const useSubscribe = () => {
-  const { subUpdateInterval, epTestInterval, socksPort } = settings.use();
+  const { epTestInterval, socksPort } = settings.use();
 
-  useEffect(() => {
-    if (subUpdateInterval > 0) {
-      console.log(`Updating subscriptions every ${subUpdateInterval} minutes.`);
-      const timer = setInterval(updateSubscriptions, subUpdateInterval * 60 * 1000);
-      return () => clearInterval(timer);
-    }
-  }, [subUpdateInterval]);
+  // useEffect(() => {
+  //   if (subUpdateInterval > 0) {
+  //     console.log(`Updating subscriptions every ${subUpdateInterval} minutes.`);
+  //     const timer = setInterval(updateSubscriptions, subUpdateInterval * 60 * 1000);
+  //     return () => clearInterval(timer);
+  //   }
+  // }, [subUpdateInterval]);
 
   useEffect(() => {
     if (epTestInterval > 0) {
@@ -26,11 +24,9 @@ const useSubscribe = () => {
         const latency = await testLatency(socksPort);
         console.debug(`Checked latency: ${latency}ms`);
 
-        // 如果当前节点不通了，则全测
+        // 如果当前节点不通了，则更新订阅
         if (latency > 30000) {
-          const all = await db.endpoints.toArray();
-          await testLatencies(all);
-          await selectFastest();
+          await updateSubscriptions();
         }
       }, epTestInterval * 60 * 1000);
 
